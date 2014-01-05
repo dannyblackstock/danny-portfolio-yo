2ff5c62198508facbf3c7cb488592f48f429bfb4
Danny Blackstock <dannyblackstock@gmail.com>
2014-01-04 21:52:15 -0800

    Loading some dynamic content with ajax

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
 console.log('\'Allo \'Allo!');
+
+$(document).ready( function(){
+    'use strict';
+
+    $('#content-container').load('main_page_contents.html', function() {
+        console.log('main_page_contents.html was loaded');
+    });
+
+    $('.hero-unit').click( function() {
+        $('#content-container').load('portfolio_item_contents.html', function() {
+            console.log('portfolio_item_contents.html was loaded');
+        });
+    });
+});
