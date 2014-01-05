08666edc493dae4ef1e88c2aabe015a276bd0e3c
Danny Blackstock <dannyblackstock@gmail.com>
2014-01-04 23:07:52 -0800

    Page content changes based on page hash

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,8 +8,30 @@ $(document).ready( function(){
     });
 
     $('.hero-unit').click( function() {
+        window.location.hash = 'portfolio_item_contents.html';
+    });
+});
+
+$(window).on('hashchange', function() {
+    'use strict';
+
+    // get hash
+    var hash = window.location.hash.substring(1);
+    console.log('hash changed to ' + hash);
+
+    // check hash
+    if (hash === '')
+    {
+        $('#content-container').load('main_page_contents.html', function() {
+            console.log('main_page_contents.html was loaded');
+        });
+    }
+
+    else if (hash === 'portfolio_item_contents.html')
+    {
         $('#content-container').load('portfolio_item_contents.html', function() {
             console.log('portfolio_item_contents.html was loaded');
+            window.location.hash = 'portfolio_item_contents.html';
         });
-    });
+    }
 });
