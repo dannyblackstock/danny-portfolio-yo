15b878d47dbd01b289714b6534d331860458d029
Danny Blackstock <dannyblackstock@gmail.com>
2014-01-05 21:43:57 -0800

    Create basic responsive homepage.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,37 +1,40 @@
 console.log('\'Allo \'Allo!');
 
-$(document).ready( function(){
-    'use strict';
-
-    $('#content-container').load('main_page_contents.html', function() {
-        console.log('main_page_contents.html was loaded');
-    });
-
-    $('.hero-unit').click( function() {
-        window.location.hash = 'portfolio_item_contents.html';
-    });
-});
-
-$(window).on('hashchange', function() {
-    'use strict';
-
+function loadContentFromHash() {
     // get hash
+    'use strict';
+
     var hash = window.location.hash.substring(1);
-    console.log('hash changed to ' + hash);
+    console.log('hash is now: ' + hash);
 
     // check hash
     if (hash === '')
     {
-        $('#content-container').load('main_page_contents.html', function() {
+        $('#ajax-content-container').load('main_page_contents.html', function() {
             console.log('main_page_contents.html was loaded');
         });
     }
 
     else if (hash === 'portfolio_item_contents.html')
     {
-        $('#content-container').load('portfolio_item_contents.html', function() {
+        $('#ajax-content-container').load('portfolio_item_contents.html', function() {
             console.log('portfolio_item_contents.html was loaded');
             window.location.hash = 'portfolio_item_contents.html';
         });
     }
+}
+
+$(document).ready( function(){
+    'use strict';
+
+    loadContentFromHash();
+
+    $('#logo').click( function() {
+        window.location.hash = 'portfolio_item_contents.html';
+    });
+});
+
+$(window).on('hashchange', function() {
+    'use strict';
+    loadContentFromHash();
 });
