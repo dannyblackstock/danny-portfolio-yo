97003bccaf21371ac804d8cace19219c0a21836b
Danny Blackstock <dannyblackstock@gmail.com>
2014-05-09 13:06:24 -0700

    Add comments and rename main_history_js.js to main.js.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,22 @@
 'use strict';
 
-var $portfolioContent = $('#contents-container');
-
 var loadContent = function(href) {
     var htmlRequest;
+    var $portfolioContent = $('#contents-container');
 
+    // if the url being looked for isn't empty
     if (href && href !== '' && href !== ' ' && href !== '/'){
+        // automatically look in the container_contents folder
         htmlRequest = 'container_contents/' + href + '.html';
     }
+    // otherwise, just load the homepage
     else {
         htmlRequest = 'container_contents/work.html';
     }
 
     $portfolioContent.load(htmlRequest, function(responseText, textStatus) {
         if (textStatus === 'error') {
+            // show the 404 page if the page could not be found
             $portfolioContent.load('404.html');
             // history.pushState({title: 'Danny Blackstock | 404'}, 'Danny Blackstock | 404', '404');
             // document.title = history.state.title;
@@ -23,27 +26,35 @@ var loadContent = function(href) {
 };
 
 console.log('document.location.pathname.substring(1): ' + document.location.pathname.substring(1));
+// check the url on initial load
 if (document.location.pathname.substring(1) !== '' && document.location.pathname.substring(1) !== '/') {
-    // load the requested page
+    // load the requested page if the url is not empty
     loadContent(document.location.pathname.substring(1));
 }
 else {
+    // if the url is empty on initial page load, just show the homepage
     loadContent('work');
 }
 
-// call replaceState at page load so that we can handle the popstate when we get back to the initial page load
+// call replaceState at initial page load so that we can handle the popstate when we get back to the initial page load
 // for chrome that calls popstate on initial load
 history.replaceState({myTag: true});
 
 // when the user clicks a link
 $('a, area').click( function(e) {
+    // prevent the normal link click behaviour from happening
     e.preventDefault();
+
+    // load the content based on the href attribute of the link
     var href = $(this).attr('href');
     loadContent(href);
+
+    // add to the history and add some data ('myTag') to ignore the initial load popstate
     history.pushState({myTag: true}, '', href);
     console.log('href: ' + href);
 });
 
+// when the user presses the back or forward buttons
 $(window).on('popstate', function(e) {
     // for chrome that calls popstate on initial load, check if my tag is present
     if (!e.originalEvent || !e.originalEvent.state.myTag) {
